9b850605ed10cb5820048b783a59ad1be213c246
Norbert Śleziak <norr.bitt@gmail.com>
2022-11-14 22:58:50 +0100

    Update GuiItem.java

--- src/main/java/pl/norbit/pluginutils/gui/GuiItem.java
+++ src/main/java/pl/norbit/pluginutils/gui/GuiItem.java
@@ -2,14 +2,17 @@ package pl.norbit.pluginutils.gui;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import pl.norbit.pluginutils.chat.Formatter;
 
 import java.util.List;
 
 @Builder
 @Getter
+@ToString
 public class GuiItem {
     @Builder.Default
     private int amount = 1, slot = -1;
@@ -24,9 +27,17 @@ public class GuiItem {
     public ItemStack getItemStack(){
         ItemStack itemStack = new ItemStack(material, amount);
         ItemMeta itemMeta = itemStack.getItemMeta();
-        if(!lore.isEmpty()) {
-            itemMeta.setLore(lore);
+
+        if(lore != null) {
+            if (!lore.isEmpty()) {
+                for (String s : lore) {
+                    Formatter.format(s);
+                }
+                itemMeta.setLore(lore);
+            }
         }
+        name = Formatter.format(name);
+
         itemMeta.setDisplayName(name);
         itemStack.setItemMeta(itemMeta);
 
